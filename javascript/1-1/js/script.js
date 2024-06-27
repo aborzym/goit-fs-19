bf1ada3ac73c439e5ada448cc05cf1c31235b499
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-06-27 18:47:50 +0200

    JavaScript Module 1 Class 1

---
+++ javascript/1-1/js/script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector("button");
+
+button.textContent = "Wonderful button";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector("button");
+    button.textContent = "Wonderful button";
+});
