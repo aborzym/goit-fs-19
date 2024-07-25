c425b4356aa8a59cd2d008c9211025b39d1487c4
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-07-25 22:23:25 +0200

    JavaScript Module 5 Class 2

---
+++ javascript/5-2/arrays.js
@@ -0,0 +1,25 @@
+const numbers = [51, -3, 27, 21, -68, 42, -37];
+
+const players = [
+    { name: "Adam",     level: 42,  role: "Warrior" },
+    { name: "Beth",     level: 1,   role: "Warrior" },
+    { name: "Cecil",    level: 7,   role: "Mage"    },
+    { name: "Dorthy",   level: 74,  role: "Tank"    },
+    { name: "Edmund",   level: 69,  role: "Paladin" },
+    { name: "Faith",    level: 21,  role: "Archer"  },
+    { name: "Greg",     level: 56,  role: "Mage"    },
+    { name: "Henry",    level: 99,  role: "Paladin" },
+];
+
+
+const posts = [
+    { id: 1, title: "JS article",           tag: ["JavaScript", "CoffeeScript"] },
+    { id: 2, title: "TS article",           tag: ["JavaScript", "TypeScript"] },
+    { id: 3, title: "Web article",          tag: ["HTML", "CSS", "JavaScript"] },
+    { id: 4, title: "Arrays article",       tag: ["JavaScript"] },
+    { id: 5, title: "Architecture article", tag: ["Java"] },
+    { id: 6, title: "ML AI article",        tag: ["Python"] },
+    { id: 6, title: "AI article",           tag: ["Python", "Julia"] },
+    { id: 7, title: "Blazingly fast",       tag: ["Rust"] },
+    { id: 8, title: "Maths article",        tag: ["Haskell", "Elixir"] },
+];
